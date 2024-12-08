6fb1b12c91ba05795a66787fb5c0ed9a75a96ddb
nevena-cvijovic <nevena.cvijovic1@gmail.com>
2024-12-08 14:12:32 +0100

    CSV file added and load data function created

--- src/breast_cancer_prediction_knn/core.clj
+++ src/breast_cancer_prediction_knn/core.clj
@@ -1,6 +1,19 @@
-(ns breast-cancer-prediction-knn.core)
+(ns breast-cancer-prediction-knn.core
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+  (:require [clojure.data.csv :as csv]
+            [clojure.java.io :as io]))
+
+
+
+(defn load-data
+  "Function for loading data from csv file"
+  [csv-file]
+  (with-open [reader (io/reader csv-file)]
+    (->> (csv/read-csv reader)
+         (mapv vec))
+    )
+  )
+
+(def csv-file "src/breast_cancer_prediction_knn/Cancerdata.csv")
+
+;(load-data csv-file)
